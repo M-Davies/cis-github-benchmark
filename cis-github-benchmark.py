545811da4e8bfb59d4b128925c50bcc6ce25a3ab
Morgan Davies <morgandavies2020@gmail.com>
2023-06-15 17:04:23 +0100

    Confirmed connection with github and args working

--- cis-github-benchmark.py
+++ cis-github-benchmark.py
@@ -2,7 +2,7 @@ import argparse
 from github import Github
 import logging
 import sys
-import datetime
+from datetime import datetime
 import platform
 
 # GLOBALS
@@ -16,7 +16,7 @@ def parseArgs():
     parser = argparse.ArgumentParser(
         prog="cis-github-benchmark",
         description="A CIS benchmark audit tool for GitHub environments, because it somehow didn't seem to exist before this project.",
-        epilog="Hosted on GitHub by @M-Davies"
+        epilog="Authored by @M-Davies. Please submit feedback and bug reports to https://github.com/M-Davies/cis-github-benchmark"
     )
     parser.add_argument(
         "-t", "--token",
@@ -25,7 +25,7 @@ def parseArgs():
     )
     parser.add_argument(
         "-u", "--url",
-        help="Root URL of the GitHub instance. Defaults to https://github.com",
+        help="Root URL of the GitHub instance. Defaults to https://api.github.com",
         default="https://api.github.com"
     )
     parser.add_argument(
@@ -47,31 +47,37 @@ def parseArgs():
         "-l", "--log",
         help="Produces a log file of verbose and non-verbose debug information. Defaults to a timestamped file in the current directory"
     )
-    ARGS = parser.parse_args()
-
+    localArgs = parser.parse_args()
+    
     # Setup logging
-    if ARGS.verbose is True:
+    if localArgs.verbose is True:
         LOGGER.setLevel(logging.INFO)
     else:
         LOGGER.setLevel(logging.WARN)
     
-    if ARGS.quiet is False:
+    if localArgs.quiet is False:
         LOG_OUTPUT_HANDLER = logging.StreamHandler(sys.stderr)
         LOG_OUTPUT_HANDLER.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
         LOGGER.addHandler(LOG_OUTPUT_HANDLER)
-    if ARGS.log is not None:
+    
+    if localArgs.log is not None:
         LOG_FILE_HANDLER = logging.FileHandler(filename=f"{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}_cis_benchmark.log", encoding="utf-8", mode="a")
         LOG_FILE_HANDLER.setFormatter(logging.Formatter("%(asctime)s: %(levelname)s: %(name)s: %(message)s"))
         LOGGER.addHandler(LOG_FILE_HANDLER)
-    return ARGS
+
+    return localArgs
 
 def benchmark():
     # Login to GitHub instance
     g = Github(base_url=ARGS.url, login_or_token=ARGS.token)
     if g.get_user().login is None:
         LOGGER.exception(f"FAILED to login to {ARGS.url} with the provided token")
-    print(g.get_organization())
+    else:
+        LOGGER.info(f"Successfully logged into {ARGS.url} as {g.get_user().login}")
+    
+    # Begin benchmark checks
+    
 
 if __name__ == "__main__":
-    parseArgs()
+    ARGS = parseArgs()
     benchmark()
