b56841e4e4737b569d2b52fdcc9b8e2a10ab2cbd
Morgan Davies <morgandavies2020@gmail.com>
2023-06-27 21:30:53 +0100

    Not working commit: Rejig code to not enumerate through marks

--- benchmarks.py
+++ benchmarks.py
@@ -5,28 +5,36 @@ from github import Github, Repository, GithubException
 G = Github
 LOGGER = logging.getLogger()
 
-def test_1_1_3_codereview(repo: Repository):
-    failureResponse = {"repo": repo.name, "branch": repo.default_branch}
+def getBranchProtections(repo: Repository):
     try:
-        branchProtections = repo.get_branch(repo.default_branch).get_protection()
-        if branchProtections.required_pull_request_reviews is None:
-            # Branch protections enabled but no PR reviews required
-            LOGGER.error(f"[{repo.full_name}] : Benchmark 1.1.3 FAILED. Pull request reviews were not required on the default branch ({repo.default_branch})")
-            failureResponse["reviews_required"] = False
-            return failureResponse
-        else:
-            # Check required approving reviews
-            if branchProtections.required_pull_request_reviews.required_approving_review_count <= 1:
-                LOGGER.error(f"[{repo.full_name}] : Benchmark 1.1.3 FAILED. Pull requests against branch {repo.default_branch} do not require 2 or more approvers before merging (approvers required = {branchProtections.required_pull_request_reviews.required_approving_review_count})")
-                failureResponse["reviewers"] = branchProtections.required_pull_request_reviews.required_approving_review_count
-                return failureResponse
-            else:
-                return True
+        return repo.get_branch(repo.default_branch).get_protection()
     except GithubException:
+        # No protections are enabled
+        return None
+
+def checkBranchProtections(repo: Repository, failureResponse: dict):
+    branchProtections = getBranchProtections(repo)
+    if branchProtections is None:
         # No protections are enabled
         LOGGER.error(f"[{repo.full_name}] : Benchmark 1.1.3 FAILED. No branch protections were enforced on the default branch ({repo.default_branch})")
         failureResponse["protection"] = False
         return failureResponse
+    elif branchProtections.required_pull_request_reviews is None:
+        # Branch protections enabled but no PR reviews required
+        LOGGER.error(f"[{repo.full_name}] : Benchmark 1.1.3 FAILED. Pull request reviews were not required on the default branch ({repo.default_branch})")
+        failureResponse["reviews_required"] = False
+        return failureResponse
+    else:
+        # Check required approving reviews
+        if branchProtections.required_pull_request_reviews.required_approving_review_count <= 1:
+            LOGGER.error(f"[{repo.full_name}] : Benchmark 1.1.3 FAILED. Pull requests against branch {repo.default_branch} do not require 2 or more approvers before merging (approvers required = {branchProtections.required_pull_request_reviews.required_approving_review_count})")
+            failureResponse["reviewers"] = branchProtections.required_pull_request_reviews.required_approving_review_count
+            return failureResponse
+        else:
+            return True
+
+def test_1_1_4_stale_approvals(repo: Repository, failureResponse: dict):
+
 
 
 def runBenchmarks(benchmarks: list, g: Github, logger: logging.getLogger(), forks: bool):
@@ -54,11 +62,19 @@ def runBenchmarks(benchmarks: list, g: Github, logger: logging.getLogger(), fork
     failedMarks = []
     for currentRepo in repos:
         LOGGER.info(f"Benchmarking {currentRepo.full_name}...")
+        # TODO: Don't need to enumerate through benchmarks, better to just pass on the valid list and pick and chose them as needed to save on processing power and time
         for currentBenchmark in benchmarks:
-            if currentBenchmark == "1.1.3" and reposPresent is True:
-                LOGGER.info(f"[{currentRepo.full_name}] : Checking benchmark 1.1.3...")
-                benchmarkResult = test_1_1_3_codereview(currentRepo)
-
+            # Check protections are alive first so we don't waste time on useless checks
+            LOGGER.info(f"[{currentRepo.full_name}] : Checking benchmark {currentBenchmark}...")
+            failureResult = {"repo": currentRepo.name, "branch": currentRepo.default_branch, "protections": getBranchProtections(currentRepo)}
+            if failureResult.protections is None:
+                LOGGER.error(f"[{currentRepo.full_name}] : Benchmark {currentBenchmark} FAILED. No branch protections were enforced on the default branch ({currentRepo.default_branch})")
+                benchmarkResult = failureResult
+            
+            elif currentBenchmark == "1.1.3" and reposPresent is True:
+                benchmarkResult = test_1_1_3_codereview(currentRepo, failureResult)
+            elif currentBenchmark == "1.1.4" and reposPresent is True:
+                benchmarkResult = test_1_1_4_stale_approvals(currentRepo, failureResult)
             # Evaluate result
             if benchmarkResult is True:
                 LOGGER.info(f"[{currentRepo.full_name}] : Benchmark {currentBenchmark} PASSED!")
